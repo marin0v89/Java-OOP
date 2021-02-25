3a8336dd9f6b511bf425149b9b6d2f6f93cb74fb
marin0v89 <xandarous89@gmail.com>
2021-02-25 19:23:04 +0200

    Working with Abstraction - Exercise - Third Problem

---
+++ src/C01_WorkingWithAbstraction/Exercise/P03_CardsWithPower/CardSuit.java
@@ -0,0 +1,17 @@
+package C01_WorkingWithAbstraction.Exercise.P03_CardsWithPower;
+
+public enum CardSuit {
+    CLUBS (0),
+    DIAMONDS (13),
+    HEARTS (26),
+    SPADES (39);
+
+    private int suitPower;
+    CardSuit(int suitPower){
+        this.suitPower = suitPower;
+    }
+
+    public int getSuitPower() {
+        return suitPower;
+    }
+}
