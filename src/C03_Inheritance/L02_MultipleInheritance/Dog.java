d009643c1b7ea9495b3805d972be31aa9ddd03a1
marin0v89 <xandarous89@gmail.com>
2021-03-04 22:02:38 +0200

    Inheritance - Lab - Second Problem

---
+++ src/C03_Inheritance/L02_MultipleInheritance/Dog.java
@@ -0,0 +1,7 @@
+package C03_Inheritance.L02_MultipleInheritance;
+
+public class Dog extends Animal{
+    public void bark(){
+        System.out.println("barking...");
+    }
+}
---
+++ src/C03_Inheritance/L02_MultipleInheritance/Puppy.java
@@ -0,0 +1,7 @@
+package C03_Inheritance.L02_MultipleInheritance;
+
+public class Puppy extends Dog{
+    public void weep(){
+        System.out.println("weeping...");
+    }
+}
