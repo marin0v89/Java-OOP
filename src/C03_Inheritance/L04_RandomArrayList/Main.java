638ac4359981b70e118ba7a5481fca2d3a4098ca
marin0v89 <xandarous89@gmail.com>
2021-03-04 22:32:30 +0200

    Inheritance - Lab - Fourth Problem

---
+++ src/C03_Inheritance/L04_RandomArrayList/Main.java
@@ -0,0 +1,9 @@
+package C03_Inheritance.L04_RandomArrayList;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/C03_Inheritance/L04_RandomArrayList/RandomArrayList.java
@@ -0,0 +1,12 @@
+package C03_Inheritance.L04_RandomArrayList;
+
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomArrayList<T> extends ArrayList<T> {
+
+    public  T getRandomElement(){
+        int index = ThreadLocalRandom.current().nextInt(super.size());
+        return remove(index);
+    }
+}
