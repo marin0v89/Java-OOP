26323dea67a83e48f9d727d0343629b6de1ca91e
marin0v89 <xandarous89@gmail.com>
2021-03-04 21:55:54 +0200

    Inheritance - Lab - First Problem

---
+++ src/C03_Inheritance/L01_SingleInheritance/Main.java
@@ -0,0 +1,11 @@
+package C03_Inheritance.L01_SingleInheritance;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+        dog.bark();
+        dog.eat();
+
+    }
+}
