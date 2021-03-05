29fe3f96bee942f983524f3675f36210dd8a0034
marin0v89 <xandarous89@gmail.com>
2021-03-05 22:26:04 +0200

    Inheritance - Exercise - Fourth Problem

---
+++ src/C03_Inheritance/E04_NeedForSpeed/CrossMotorcycle.java
@@ -0,0 +1,7 @@
+package C03_Inheritance.E04_NeedForSpeed;
+
+public class CrossMotorcycle extends Motorcycle {
+    public CrossMotorcycle(double fuel, int horsepower) {
+        super(fuel,horsepower);
+    }
+}
