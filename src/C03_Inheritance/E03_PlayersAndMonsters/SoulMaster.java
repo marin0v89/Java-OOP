b13d85729404f06565d0384fa0cbc3a1729f1743
marin0v89 <xandarous89@gmail.com>
2021-03-05 16:40:40 +0200

    Inheritance - Exercise - Third Problem

---
+++ src/C03_Inheritance/E03_PlayersAndMonsters/SoulMaster.java
@@ -0,0 +1,7 @@
+package C03_Inheritance.E03_PlayersAndMonsters;
+
+public class SoulMaster extends DarkWizard {
+    public SoulMaster(String userName, int level) {
+        super(userName, level);
+    }
+}
