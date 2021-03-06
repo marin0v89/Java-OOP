66c25a5657df6da68f822c2dc662b1ab532e0b61
marin0v89 <xandarous89@gmail.com>
2021-03-06 15:53:57 +0200

    Inheritance - Exercise - Fifth Problem

---
+++ src/C03_Inheritance/E05_Restaurant/Tea.java
@@ -0,0 +1,9 @@
+package C03_Inheritance.E05_Restaurant;
+
+import java.math.BigDecimal;
+
+public class Tea extends HotBeverage {
+    public Tea(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}
