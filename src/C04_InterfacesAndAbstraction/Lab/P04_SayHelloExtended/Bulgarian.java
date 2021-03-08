a727ef2a8a2da00f17c6304b05221330c32d8e2c
marin0v89 <xandarous89@gmail.com>
2021-03-08 19:05:33 +0200

    Interfaces And Abstraction - Lab - Fourth Problem

---
+++ src/C04_InterfacesAndAbstraction/Lab/P04_SayHelloExtended/Bulgarian.java
@@ -0,0 +1,12 @@
+package C04_InterfacesAndAbstraction.Lab.P04_SayHelloExtended;
+
+public class Bulgarian extends BasePerson{
+    protected Bulgarian(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Здравей";
+    }
+}
