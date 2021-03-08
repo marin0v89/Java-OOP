ffc12fe455810972c4ec0af7253d724adb004e86
marin0v89 <xandarous89@gmail.com>
2021-03-08 19:05:25 +0200

    Interfaces And Abstraction - Lab - Fourth Problem

---
+++ src/C04_InterfacesAndAbstraction/Lab/P04_SayHelloExtended/BasePerson.java
@@ -0,0 +1,22 @@
+package C04_InterfacesAndAbstraction.Lab.P04_SayHelloExtended;
+
+public abstract class BasePerson implements Person {
+    private String name;
+
+    protected BasePerson(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+    @Override
+    public String sayHello(){
+        return "Hello";
+    }
+}
