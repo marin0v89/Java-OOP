8999d538d959ea839301124fd81963530a7bf74e
marin0v89 <xandarous89@gmail.com>
2021-03-08 19:05:41 +0200

    Interfaces And Abstraction - Lab - Fourth Problem

---
+++ src/C04_InterfacesAndAbstraction/Lab/P04_SayHelloExtended/Chinese.java
@@ -0,0 +1,12 @@
+package C04_InterfacesAndAbstraction.Lab.P04_SayHelloExtended;
+
+public class Chinese extends BasePerson {
+    protected Chinese(String name) {
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Djydjybydjy";
+    }
+}
