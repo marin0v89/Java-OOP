84b11867959e52c35dc77f68957ea7cdf6d9d1b9
marin0v89 <xandarous89@gmail.com>
2021-03-10 17:40:06 +0200

    Interfaces And Abstraction - Lab - Fourth Problem

---
+++ src/C04_InterfacesAndAbstraction/Exercise/P04_FoodShortage/Citizen.java
@@ -0,0 +1,46 @@
+package C04_InterfacesAndAbstraction.Exercise.P04_FoodShortage;
+
+public class Citizen implements Birthable, Buyer, Identifiable, Person {
+    private String name;
+    private int age;
+    private String id;
+    private String birthDate;
+    private int food;
+
+    public Citizen(String name, int age, String id, String birthDate) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getAge() {
+        return this.age;
+    }
+
+    @Override
+    public String getBirthDate() {
+        return this.birthDate;
+    }
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+
+    @Override
+    public int getFood() {
+        return this.food;
+    }
+
+    @Override
+    public void buyFood() {
+        this.food = getFood() + 10;
+    }
+}
