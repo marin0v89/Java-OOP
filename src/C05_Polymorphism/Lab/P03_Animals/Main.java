44546508a1f2e00b6252a650841018655058cda0
marin0v89 <xandarous89@gmail.com>
2021-03-12 16:50:01 +0200

    Polymorphism - Lab - Second Problem

---
+++ src/C05_Polymorphism/Lab/P03_Animals/Main.java
@@ -0,0 +1,11 @@
+package C05_Polymorphism.Lab.P03_Animals;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal cat = new Cat("Oscar", "Whiskas");
+        Animal dog = new Dog("Rocky", "Meat");
+        System.out.println(cat.explainSelf());
+        System.out.println(dog.explainSelf());
+    }
+
+}
