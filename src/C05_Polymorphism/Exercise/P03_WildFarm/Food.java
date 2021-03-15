eef4d842112712b49e67887151260ca3f31a67e6
marin0v89 <xandarous89@gmail.com>
2021-03-15 17:53:32 +0200

    Polymorphism - Exercise - Third Problem

---
+++ src/C05_Polymorphism/Exercise/P03_WildFarm/Food.java
@@ -0,0 +1,13 @@
+package C05_Polymorphism.Exercise.P03_WildFarm;
+
+public abstract class Food {
+    private Integer quantity;
+
+    public Food(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+}
