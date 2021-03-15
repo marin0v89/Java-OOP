497d03a409fce1c49a03718e103fce5c342c1e5e
marin0v89 <xandarous89@gmail.com>
2021-03-15 17:53:52 +0200

    Polymorphism - Exercise - Third Problem

---
+++ src/C05_Polymorphism/Exercise/P03_WildFarm/Mammal.java
@@ -0,0 +1,8 @@
+package C05_Polymorphism.Exercise.P03_WildFarm;
+
+public abstract class Mammal extends Animal{
+
+    public Mammal(String animalName, String animalType, Double animalWeight, String livingRegion) {
+        super(animalName, animalType, animalWeight, livingRegion);
+    }
+}
