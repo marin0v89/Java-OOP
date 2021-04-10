163b05fbcf175ef91a97c45c4c3cc7f4646cbeac
marin0v89 <xandarous89@gmail.com>
2021-04-10 13:48:00 +0300

    Practical exam for the course

---
+++ src/Exam/aquarium/common/Command.java
@@ -0,0 +1,12 @@
+package Exam.aquarium.common;
+
+public enum Command {
+    AddAquarium,
+    AddDecoration,
+    InsertDecoration,
+    AddFish,
+    FeedFish,
+    CalculateValue,
+    Report,
+    Exit
+}
